e26d65349d8eddbfb79300d785348226bdeaa646
omerbselvi <omerbugra97@hotmail.com>
2018-10-13 22:08:16 +0300

    Add human submission form

---
+++ src/main/java/com/omerbselvi/AddNewHumanView.java
@@ -0,0 +1,74 @@
+package com.omerbselvi;
+
+import javax.faces.application.Application;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "addNewHuman")
+@SessionScoped
+public class AddNewHumanView {
+    private String name;
+    private String surname;
+    private int age;
+    private String phoneNumber;
+    private String address;
+
+    @ManagedProperty(value = "#{humanView}")
+    private HumanView humanView;
+
+    //TODO IMPROVE THIS METHOD
+    public String addHumanAction(){
+        Human newHumanToAdd = new Human(name, surname, age, phoneNumber, address);
+        humanView.getHumanList().add(newHumanToAdd);
+        return "index.xhtml";
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public HumanView getHumanView() {
+        return humanView;
+    }
+
+    public void setHumanView(HumanView humanView) {
+        this.humanView = humanView;
+    }
+}
